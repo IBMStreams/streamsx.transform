31cf61ff56b63c79f2e98a2b6ee85ee939306c21
KIRSTEN W. HILDRUM <hildrum@us.ibm.com>
2015-09-11 21:54:58 -0400

    Fix extra close comment, clean up performance tests, add new sample

--- tests/RuntimeFilter/timing/examples.sh
+++ tests/RuntimeFilter/timing/examples.sh
@@ -1,9 +1,9 @@
 toolkit=../../../com.ibm.streamsx.transform
 opts=-a
-iterations=60000000
-sc $opts -T -M StringListBasic_String1 String1 '\"malfoy\" in potterList' -t $toolkit
+iterations=300000000
+sc $opts -T -M StringListBasic_String1 String1 '\"Eyre\" in characterList' -t $toolkit
 time output/bin/standalone iterations=$iterations
-sc $opts -T -M StringListDynamic_String1 String1 '\"malfoy\" in potterList' -t $toolkit
+sc $opts -T -M StringListDynamic_String1 String1 '\"Eyre\" in characterList' -t $toolkit
 time output/bin/standalone iterations=$iterations
 sc $opts -T -M IntDynamic_1 1 'x > 5l' -t $toolkit
 time output/bin/standalone iterations=$iterations
